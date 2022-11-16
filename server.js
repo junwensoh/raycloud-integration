fcaa427b35a4b7032faaf4dc1e993db58bddefd7
Soh Jun Wen <junwen.soh@thecarousell.com>
2022-11-16 10:11:55 +0800

    Test change commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello from Node, from inside a docker container...</h1>");
+  res.send("<h1>Hello from Node, from inside a docker container.</h1>");
 });
 
 app.listen(process.env.PORT || 8080, () => {
