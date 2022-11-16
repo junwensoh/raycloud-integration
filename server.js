677891e671bcf95799a657802385e0ed0d0f9883
Soh Jun Wen <junwen.soh@thecarousell.com>
2022-11-16 10:33:16 +0800

    Add endpoint for raycloud inventory and price webhooks

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.send("<h1>Hello from Node, from inside a docker container.</h1>");
+});
+
+app.post("/api/raycloud/product", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  res.json({ code: "0", message: "成功", success: true });
 });
 
 app.listen(process.env.PORT || 8080, () => {
