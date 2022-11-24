f7bc1caa9938fb1e0463fe4ce9d38c4b678dc008
Soh Jun Wen <junwen.soh@thecarousell.com>
2022-11-25 02:44:01 +0800

    Create subfolder for services and implement them

---
+++ services/ShopifyService/GetInventoryLevelBySkuNumber.js
@@ -0,0 +1,61 @@
+const Shopify = require("@shopify/shopify-api").Shopify;
+
+class GetInventoryLevelBySkuNumber {
+  /**
+   * Gets the inventoryLevelId and current available quantity at a location
+   * @param  {[String]} sku The sku number of the product variant
+   * @return {[Object]} Example:
+   * {
+   *      id: 'gid://shopify/InventoryLevel/97918943321?inventory_item_id=42422268264537',
+   *      available: 3
+   * }
+   */
+  static async run(sku) {
+    try {
+      console.log('run GetInventoryLevelBySkuNumber')
+        
+      const client = new Shopify.Clients.Graphql(
+        process.env.SHOPIFY_SHOP,
+        process.env.SHOPIFY_ACCESS_TOKEN
+      );
+
+      const response = await client.query({
+        data: `
+            {
+                productVariants(first: 1, query: "sku:${sku}") {
+                    edges {
+                    node {
+                        id
+                        inventoryItem {
+                        id
+                        inventoryLevel(locationId: "gid://shopify/Location/${process.env.OXLUXE_LOCATION_ID}") {
+                            id
+                            available
+                        }
+                        }
+                    }
+                    }
+                }
+            }
+        `,
+      });
+
+      if (response.body.data.productVariants.edges.length > 0) {
+        const inventoryLevel = response.body.data.productVariants.edges[0].node.inventoryItem.inventoryLevel;
+        return inventoryLevel;
+      }
+      return undefined;
+    } catch (error) {
+      if (error instanceof Shopify.Errors.GraphqlQueryError) {
+        throw new Error(
+          `${error.message}\n${JSON.stringify(error.response, null, 2)}`
+        );
+      } else {
+        // handle other errors
+        console.log(errors);
+      }
+    }
+  }
+}
+
+module.exports = GetInventoryLevelBySkuNumber;
---
+++ services/ShopifyService/UpdateProductVariantQuantity.js
@@ -0,0 +1,71 @@
+const Shopify = require("@shopify/shopify-api").Shopify;
+const GetInventoryLevelBySkuNumber = require('./GetInventoryLevelBySkuNumber');
+
+class UpdateProductVariantQuantity {
+  static async run(sku, newInventoryLevelQuantity) {
+    try {
+      console.log(sku);
+      console.log(newInventoryLevelQuantity);
+      const client = new Shopify.Clients.Graphql(
+        process.env.SHOPIFY_SHOP,
+        process.env.SHOPIFY_ACCESS_TOKEN
+      );
+      
+      // we cannot update available quantity directly using ProductVariant and need to update it through InventoryLevel
+      // hence, getting the inventoryLevelId from a graphQL query from line below to be used on next query.
+      const inventoryLevel = await GetInventoryLevelBySkuNumber.run(sku);
+      console.log(inventoryLevel);
+      if (inventoryLevel) {
+          const availableDelta = newInventoryLevelQuantity - inventoryLevel.available;
+  
+          const response = await client.query({
+              data: {
+                  query: `mutation AdjustInventoryQuantity($input: InventoryAdjustQuantityInput!) {
+                      inventoryAdjustQuantity(input: $input) {
+                        inventoryLevel {
+                          id
+                          available
+                          incoming
+                          item {
+                            id
+                            sku
+                          }
+                          location {
+                            id
+                            name
+                          }
+                        }
+                      }
+                    }
+                  `,
+                  variables: {
+                      input: {
+                          inventoryLevelId: inventoryLevel.id,
+                          availableDelta: availableDelta
+                      }
+                  }
+              }
+          });
+          
+          const newAvailableQuantity = response.body.data.inventoryAdjustQuantity.inventoryLevel.available;
+          console.log(newAvailableQuantity);
+          console.log(`sku number: ${sku} has been updated with new quantity: ${newAvailableQuantity}`);
+      } else {
+          console.log(`no updates has been made for sku: ${sku} as it is not found on Shopify.`);
+      }
+    } catch (error) {
+      if (error instanceof Shopify.Errors.GraphqlQueryError) {
+        throw new Error(
+          `${error.message}\n${JSON.stringify(error.response, null, 2)}`
+        );
+      } else {
+        // handle other errors
+        console.log(errors);
+      }
+    }
+    return;
+  }
+}
+
+module.exports = UpdateProductVariantQuantity;
+
