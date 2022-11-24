cfae91b74a5fe4e9b02b9a6f0bc704143d90ba09
Soh Jun Wen <junwen.soh@thecarousell.com>
2022-11-25 03:54:26 +0800

    Fix TypeError

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -4,9 +4,8 @@ exports.updateProductInfo = async (req, res) => {
   try {
     console.log("=====Product inventory/price update received from Raycloud webhook=====");
     console.log(req.body);
-    const productUpdates = req.body.data;
-    console.log(productUpdates);
-
+    const productUpdates = JSON.parse(req.body.data)[0];
+    
     res.json({ code: "0", message: "成功", success: true });
 
     if ("quantity" in productUpdates) {
